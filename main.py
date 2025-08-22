5b272df4e8cefa8bf0a01f35b909e734db39851a
The-Vic-Rattlehead <hannankamran4@gmail.com>
2025-08-22 15:04:06 -0600

    Changed some logic and added modifers for foods, added a while loop if a user enters a dish or something that is not an option given on the list then then user is propmt to give an actual answer on the list

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from pydantic import BaseModel
 from rapidfuzz import process, fuzz
 import json
 
-number_people=7#input("# of people")
-ingredients="basmatti rice, 3 chicken breast, indian spices, 5 tomatoes, 1 red onion, 1 whole garlic, 1 ginger "#input("ingredients with amounts")
+number_people=input("# of people: ")
+ingredients=input("ingredients with amounts: ")
+modifers=input("Culinary Style, Nutritional Requirments, Preperation Method, Alergies, Other Modifiers")
+
+if modifers!="":    
+    prompt=f"""I need suggestions for dishes for this many people: {number_people}, 
+    I have these indredients with their ammounts: {ingredients},
+    I have these requirments of the dish as well: {modifers},
+    show ammounts as well, assume user has basic spices or also include alternative spices where possible, 
+    show me at leaste more than 3 dishes but less than 10"""
+else:
+    prompt=f"""i need suggestions for dishes for this many people: {number_people}, 
+    i have these indredients with their ammounts: {ingredients}, 
+    show ammounts as well, assume user has basic spices or also include alternative spices where possible, 
+    show me at leaste more than 3 dishes but less than 10"""
 
 class Dish(BaseModel):
     dish_name: str
@@ -12,8 +25,8 @@ class Dish(BaseModel):
 
 client = genai.Client()
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"i need suggestions for dishes for this many people: {number_people}, i have these indredients with their ammounts: {ingredients}, show ammounts as well,assume user has basic spices or also include alternative spices where possible, show me at leaste more than 3 dishes but less than 10",
+    model="gemini-2.5-flash-lite",
+    contents=prompt,
     config={
         "response_mime_type": "application/json",
         "response_schema": list[Dish],
@@ -22,26 +35,30 @@ response = client.models.generate_content(
 
 # Use the response as a JSON string.
 print(response.text)
+
 data=json.loads(response.text)
 
-dish=input("From list choose dish")
+valid_dish=False
+while valid_dish==False:
+    dish=input("From list choose dish: ")
 
-choices = [d["dish_name"] for d in data]
-best_match, score, idx = process.extractOne(
-    dish, choices, scorer=fuzz.WRatio
-)
+    choices = [d["dish_name"] for d in data]
+    best_match, score, idx = process.extractOne(
+        dish, choices, scorer=fuzz.WRatio
+    )
 
-if score < 60:  # threshold, tweak as needed
-    print("Couldn't find a close enough match. Try again.")
-else:
-    selected = data[idx]
-    dish_name = selected["dish_name"]
-    ingredients = selected["valid_ingreients"]
+    if score < 60:  # threshold, tweak as needed
+        print("Couldn't find a close enough match. Try again.")
+    else:
+        selected = data[idx]
+        dish_name = selected["dish_name"]
+        ingredients = selected["valid_ingreients"]
 
-    print(f"\nYou picked: {dish_name} (match score: {score})")
-    print("Ingredients:")
-    for item in ingredients:
-        print(f" - {item}")
+        print(f"\nYou picked: {dish_name} (match score: {score})")
+        print("ingredients:")
+        for item in ingredients:
+            print(f" - {item}")
+        valid_dish=True
 
     
 print(dish_name, ingredients)
@@ -52,8 +69,10 @@ class Recipe(BaseModel):
 
 client = genai.Client()
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"Ive choosen {dish} for {number_people} people, with only these {ingredients}, please give the enitre recipe",
+    model="gemini-2.5-flash-lite",
+    contents=f"""Ive choosen {dish_name} for {number_people} people, with only these {ingredients}, 
+    if there are extra ingredients that the user does not state to have then do not try to use them only stick to ingredients they have entered or state them as extra needed ingredients, 
+    give the enitre recipe""",
     config={
         "response_mime_type": "application/json",
         "response_schema": list[Recipe],
