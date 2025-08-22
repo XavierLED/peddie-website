fa257f6f1a2c1463f6a10b1bcab346a2f8585cbb
Xavier Bester <xavierbester6@gmail.com>
2025-08-22 10:55:12 -0600

    adding todo

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 
 client = genai.Client(api_key= os.environ.get("API_Key"))
 
+##TODO finding the users ingredients and how many people to serve
+
+
+
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Make a spagetti recepi"
 )
