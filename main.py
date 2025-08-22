20826a6e02d403f2d28de4d1e390a14a0e4a909d
Xavier Bester <xavierbester6@gmail.com>
2025-08-22 10:45:53 -0600

    adding ai to project

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from google import genai
 import os
+
+client = genai.Client(api_key= os.environ.get("API_Key"))
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Make a spagetti recepi"
+)
+print(response.text)
