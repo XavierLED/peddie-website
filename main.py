5dc2cd7893147377af56000d7b7a0e2e7994445f
Xavier Bester <xavierbester6@gmail.com>
2025-08-23 14:32:42 -0600

    loading works

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from google import genai
 from pydantic import BaseModel 
-from rapidfuzz import process, fuzz
 import json
 
 app = Flask(__name__, template_folder='html', static_folder='static', static_url_path='/')
 
-mylist = "" 
-mods = ""
-number = ""
-response = ""
-recipe = ""
+app.config['SECRET_KEY'] = 'your-secret-key-here'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global mylist, mods, number, response, recipe
     if request.method == 'GET':
-        return render_template('index.html', mylist=mylist, mods=mods, number=number, response=response, recipe=recipe)
+        session.clear()
+        
+    if 'mylist' not in session:
+            session['mylist'] = ""
 
+    if 'mods' not in session:
+        session['mods'] = ""
+
+    if 'number' not in session:
+        session['number'] = ""
+
+    if 'response' not in session:
+        session['response'] = ""
+
+    if 'recipe' not in session:
+        session['recipe'] = ""
     else:
             
             # Check if form is empty (might indicate a different issue)
 
         if 'button_ingredient' in request.form:
             if request.form['ingredients']:
-                mylist=[request.form['ingredients']]
+                session['mylist']=[request.form['ingredients']]
             else:
-                mylist = ""
+                session['mylist'] = ""
 
         elif 'button_mods' in request.form:
             if request.form['mod']:
-                mods=[request.form['mod']]
+                session['mods']=[request.form['mod']]
             else:
-                mods = ""
+                session['mods'] = ""
             
         elif 'button_number' in request.form:
             if request.form['number_people']:
-                number = request.form['number_people']
+                session['number'] = request.form['number_people']
             else:
-                number = ""
+                session['number'] = ""
 
         elif 'button_generate' in request.form:
-            if mylist != "" and number != "":
-                response = get_dishes(number, mylist, mods)
+            if session['mylist'] != "" and session['number'] != "":
+                session['response'] = get_dishes(session['number'], session['mylist'], session['mods'])
 
         elif 'this_dish' in request.form:
-            recipe = pick_dish(request.form['this_dish'],response,number)
+            session['recipe'] = pick_dish(request.form['this_dish'],session['response'],session['number'])
 
-        return render_template('index.html', mylist=mylist, mods=mods, response=response, number=number, recipe=recipe)
+    return render_template('index.html', mylist=session['mylist'], mods=session['mods'], number=session['number'], response=session['response'], recipe=session['recipe'])
 
 
 def get_dishes(number_people, ingredients, modifers):
